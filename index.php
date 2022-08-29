d5915fdac68ca1e9d7e43a706516c0d1aa589b0c
Shira Zacks <shiramzacks@gmail.com>
2022-08-29 16:37:06 -0400

    maaser website

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Maaser Calculations</title>
+</head>
+<body>
+    <h1 class="title">Welcome to the Maaser Calculations Website!</h1>
+    <h2 class="title">You do the money, we do the math</h2>
+    <br>
+    
+</body>
+</html>
