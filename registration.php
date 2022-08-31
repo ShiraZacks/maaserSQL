920a2e702d680b3ccfcfea798ddc2e6a871f2a75
Shira Zacks <shiramzacks@gmail.com>
2022-08-30 23:09:05 -0400

    formatting edit

--- registration.php
+++ registration.php
@@ -1,17 +1,19 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-    <meta charset="utf-8"/>
+    <meta charset="utf-8" />
     <title>Registration</title>
-    <link rel="stylesheet" href="style.css<?php time()?>" />
+    <link rel="stylesheet" href="style.css<?php time() ?>" />
 </head>
+
 <body>
-<?php
+    <?php
     require 'dbconnect1.php';
     session_start();
     $errors = array();
     // When form submitted, insert values into the database.
-    if(isset($_REQUEST['username'])) {
+    if (isset($_REQUEST['username'])) {
         //escapes special characters in a string
         $username = stripslashes($_REQUEST['username']);    // removes backslashes
         $username = mysqli_real_escape_string($conn, $username);
@@ -21,13 +23,13 @@
         $confirmPassword = mysqli_real_escape_string($conn, $confirmPassword);
 
         //form validation
-        if(empty($username)) {
+        if (empty($username)) {
             array_push($errors, "Username is required");
         }
-        if(empty($password)) {
+        if (empty($password)) {
             array_push($errors, "Password is required");
         }
-        if($password != $confirmPassword){
+        if ($password != $confirmPassword) {
             array_push($errors, "Passwords do not match");
         }
 
@@ -35,15 +37,15 @@
         $user_check_query = "SELECT * FROM maaser WHERE username= '$username' LIMIT 1";
         $result = mysqli_query($conn, $user_check_query);
         $user = mysqli_fetch_assoc($result);
-        if($user){
-            if($user['username'] === $username){
+        if ($user) {
+            if ($user['username'] === $username) {
                 array_push($errors, "Username already exists");
             }
         }
         mysqli_free_result($result);
         //Only insert if there are no errors
-        if(count($errors)== 0){
-           //after error checks  we will now insert
+        if (count($errors) == 0) {
+            //after error checks  we will now insert
             $query    = "INSERT INTO maaser (username, password) VALUES ('$username', '" . md5($password) . "')";
             $result   = mysqli_query($conn, $query);
             if ($result) {
@@ -55,15 +57,13 @@
                     <h3>Click here to register another user.</h3><br/>
                     <p class='link'>Click here to <a href='registration.php'>Register another user</a></p>
                     </div>";
-            } 
-            else{
-                    echo "<div class='form'>
+            } else {
+                echo "<div class='form'>
                     <h3>Something went wrong with the connection.</h3><br/>
                     <p class='link'>Click here to <a href='registration.php'>register</a> again.</p>
                     </div>";
             }
-        }
-        else{           
+        } else {
             echo "<div class='form'>
                     <h3>You have errors:<br>
                             *required fields are missing<br>
@@ -71,28 +71,32 @@
                             *username already exists.</h3><br/>
                     <p class='link'>Click here to <a href='registration.php'>try</a> again.</p>
                     </div>";
-            }
         }
-    else {
-?>
-    <form action="registration.php" method="post">
+    } else {
+    ?>
+        <form action="registration.php" method="post">
 
-        <p><h1>Registration</h1> 
+            <p>
+            <h1>Registration</h1>
 
-        <p><label for="username">Username : </label>
-            <input type="text" name="username" required></p>
-        
-        <p><label for="password">Password : </label>
-            <input type="password" name="password"required></p>
-        
-        <p><label for="confirmPassword">Confirm Password : </label>
-            <input type="password" name="confirmPassword"required></p>
-        
-        <p><button type="submit" name="registration"> Submit </button></p>
-        <p>Already a user? <a href="login.php"><b>Login</b></a></p>
-    </form>
-<?php
+            <p><label for="username">Username : </label>
+                <input type="text" name="username" required>
+            </p>
+
+            <p><label for="password">Password : </label>
+                <input type="password" name="password" required>
+            </p>
+
+            <p><label for="confirmPassword">Confirm Password : </label>
+                <input type="password" name="confirmPassword" required>
+            </p>
+
+            <p><button type="submit" name="registration"> Submit </button></p>
+            <p>Already a user? <a href="login.php"><b>Login</b></a></p>
+        </form>
+    <?php
     }
-?>
+    ?>
 </body>
+
 </html>
