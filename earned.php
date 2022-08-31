7d8bc088256086a49082e0654e701a908e55ac5f
Shira Zacks <shiramzacks@gmail.com>
2022-08-30 22:36:03 -0400

    earned.php

---
+++ earned.php
@@ -0,0 +1,8 @@
+<!-- 
+*here we put money people earned. we could have the stuff from temporary page here.
+*need place to enter reason, money, and then puts it in a chart.
+*chart should have an addition feature that adds everything up and should also have a
+*division thing so it gives how much maaser one owes.
+-->
+
+<!-- maybe have only one page because want the data to be in one page... -->
