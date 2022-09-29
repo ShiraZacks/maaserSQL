23f13ddd4df0e228f3fc6c14d907d458a8fd44e0
Shira Zacks <shiramzacks@gmail.com>
2022-09-29 11:23:27 -0400

    changed index

--- index.php
+++ index.php
@@ -1,18 +1,88 @@
+<?php 
+    // initialize errors variable
+	$errors = "";
+
+	// connect to database
+	$db = mysqli_connect("localhost", "root", "", "maaser");
+
+	// insert to db if submit button is clicked
+	if (isset($_POST['submit'])) {
+			if ((empty($_POST['source'])&&($_POST['date_earned'])&&($_POST['earned']))||((empty($_POST['recipient'])&&($_POST['date_given'])&&($_POST['given'])))) {
+				$errors = "Looks like you left something out...";
+			}else{
+					//things to submit
+			$source = $_POST['source'];
+			$date_earned = $_POST['date_earned'];
+			$earned = $_POST['earned'];
+			$recipient = $_POST['recipient'];
+			$date_given = $_POST['date_given'];
+			$given = $_POST['given'];
+					//make it submit
+			$sql = "INSERT INTO maaser (source, date_earned, earned, recipient, date_given, given) VALUES ('$source', '$date_earned', '$earned', '$recipient', '$date_given', '$given')";
+
+			mysqli_query($db, $sql);
+			header('location: index.php');
+	}
+	};
+
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Maaser Calculator</title>
+	<title>Maaser Tracker</title>
+    <link rel="stylesheet" type="text/css" href="style.css?<?=time()?>">
 </head>
 <body>
-          
-        </div>
-        <!-- if the user logs in print information about the user  -->
+	<div class="heading">
+		<h2 style="font-style: 'Hervetica';">Maaser Tracker</h2>
+	</div>
+	<form method="post" action="index.php" class="input_form">
+	<?php if (isset($errors)) { ?>
+	<p><?php echo $errors; ?></p>
+	<?php } ?>
+        <p> Source: <input type="text" name="source" class="task_input"></p>
+		<p>Date Earned: <input type="text" name="date_earned"	class="task_input"></p>
+		<p>Amount Earned: <input type="text" name="earned"	class="task_input"></p>
+		<p>Recipient: <input type="text" name="recipient"	class="task_input"></p>
+		<p>Date Given: <input type="text" name="date_given"	class="task_input"></p>
+		<p>Amount Given: <input type="text" name="given"	class="task_input"></p>
+		<button type="submit" name="submit" id="add_btn" class="add_btn">Add to Database</button>
+	</form>
+    
+<table>
+	<thead>
+		<tr>
+			<th>ID</th>
+			<th>Source</th>
+			<th>Date Earned</th>
+			<th>Amount Earned</th>
+			<th>Maaser Owed</th>
+			<th>Recipient</th>
+			<th>Date Given</th>
+			<th>Amount Given</th>
+		</tr>
+	</thead>
 
-            <h3>Welcome <strong> Maaser User! </strong></h3><br>
-            <p>this is a maaser tracking application. Use it well.</p>
-    </body>
+	<tbody>
+		<?php 
+		// select all tasks if page is visited or refreshed
+		$maaser = mysqli_query($db, "SELECT * FROM maaser");
+
+		$i = 1; while ($row = mysqli_fetch_array($maaser)) { ?>
+			<tr>
+				<td> <?php echo $i; ?> </td>
+				<td class="id"><?php echo $row['id'];?></td>
+				<td class="source"><?php echo $row['source'];?></td>
+				<td class="date_earned"><?php echo $row['date_earned'];?></td>
+				<td class="earned"><?php echo $row['earned'];?></td>
+				<td class="maaser_owed"><?php echo $row['maaser_owed'];?></td>
+				<td class="recipient"><?php echo $row['recipient'];?></td>
+				<td class="date_given"><?php echo $row['source'];?></td>
+				<td class="given"><?php echo $row['given']; ?></td>
+			</tr>
+		<?php $i++; } ?>	
+	</tbody>
+</table>
 </body>
 </html>
